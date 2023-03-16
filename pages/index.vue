1ab516425d825eacd4e14275e65266a5b4aea825
Giorgos Kalmoukis <giorgoskalmoukis@gmail.com>
2023-03-16 18:02:42 +0200

    Add homepage intro text

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,14 @@
 <template>
-    
+    <h1>Hi, </h1>
+    <p>
+        Welcome to our website! 
+        We offer a wide selection of calculators to help you with your everyday tasks. 
+        <br>
+        Our calculators are designed to save you time and money, and are tailored to meet your specific needs. 
+        Our site is optimized for search engines, so you can easily find the calculator you need. 
+        <br>
+        Thanks for visiting our site and we hope you find it useful!
+    </p>
 </template>
 
 <script>
