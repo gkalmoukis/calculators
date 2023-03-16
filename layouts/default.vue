48476cac6d382d5f7760093260735eb64ad6933e
Giorgos Kalmoukis <giorgoskalmoukis@gmail.com>
2023-03-16 17:51:12 +0200

    Add navigation

---
+++ layouts/default.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <nav>
+            <ul>
+                <li><nuxt-link to="/">Home</nuxt-link></li>
+                <li><nuxt-link to="/percentange-chgange">Percentange Chgange</nuxt-link></li>
+            </ul>
+        </nav>
+    </div>
+    <NuxtPage />
+</template>
